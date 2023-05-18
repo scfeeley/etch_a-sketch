c5dbef4b853fc44bff4ad0075bd6cbbb130c3dcd
Shannon Feeley <scfeeley@wpi.edu>
2023-05-18 16:18:31 -0600

    grid resize feature added/removed old code

--- draw.js
+++ draw.js
@@ -1,91 +1,60 @@
 let drawing = false;
-let width = 16;
-let maxWidth = 50;
-/**********************************
-User Interface
- *********************************/
-//const container = document.querySelector('.container');
+let maxWidth = 100;
+let minWidth = 16;
+let startWidth = 16;
+
 const container = document.querySelector('.drawing');
+const size = document.getElementById('size');
+const colors = document.getElementById('colors');
 
-const grid = document.createElement('div');
-grid.classList.add('grid');
-let squares = [];
-let rows =[];
-for(let i = 0; i < maxWidth; i++){
-    squares.push([]);
-    rows.push(document.createElement('div'));
-    rows[i].classList.add('row');
-    for(let j = 0; j < maxWidth; j++){
-      squares[i].push(document.createElement('div'));
-      squares[i][j].classList.add('square');
-      //if in first row add class top
-      if(i === 0){
-        squares[i][j].classList.add('top');
-      }
-      //if in last column add class right
-      if(j + 1 === width){
-        squares[i][j].classList.add('right');
-      }
-      if(j >= width || i >= width){
-        squares[i][j].classList.add('hide');
-      }
-      //If mouse down, start drawing
-      squares[i][j].addEventListener('mousedown', ()=>{
-        drawing = true;
-        squares[i][j].classList.add('select');
-        squares[i][j].style.backgroundColor = colors.value;
-      })
-      //If mouse enters square while drawing, keep drawing
-      squares[i][j].addEventListener('mouseenter', () =>{
-        if(drawing){
-          squares[i][j].classList.add('select');
-          squares[i][j].style.backgroundColor = colors.value;
-      }
-    });
-      rows[i].appendChild(squares[i][j]);
+//Creates grid with specified width
+function createGrid(width){
+    let grid = document.getElementById('grid');
+    grid.classList.add('grid');
+    let squares = [];
+    let rows =[];
+    for(let i = 0; i < width; i++){
+        squares.push([]);
+        rows.push(document.createElement('div'));
+        rows[i].classList.add('row');
+        for(let j = 0; j < width; j++){
+          squares[i].push(document.createElement('div'));
+          squares[i][j].classList.add('square');
+          squares[i][j].dataset.row = i;
+          squares[i][j].dataset.column =j;
+
+          //If mouse down, start drawing
+          squares[i][j].addEventListener('mousedown', ()=>{
+            drawing = true;
+            squares[i][j].classList.add('select');
+            squares[i][j].style.backgroundColor = colors.value;
+          });
+
+          //If mouse enters square while drawing, keep drawing
+          squares[i][j].addEventListener('mouseenter', () =>{
+            if(drawing){
+              squares[i][j].classList.add('select');
+              squares[i][j].style.backgroundColor = colors.value;
+          }});
+
+
+          //if mouse is released, stop drawing
+          squares[i][j].addEventListener('mouseup', ()=>{
+            drawing=false;
+          });
+
+          rows[i].appendChild(squares[i][j]);
+        }
+
+        grid.appendChild(rows[i]);
+
     }
-    grid.appendChild(rows[i]);
 }
-container.appendChild(grid)
 
+//initialize grid
+createGrid(startWidth);
 
-//create toolbar
-//const tools = document.createElement('div');
-//tools.classList.add('tools');
-
-//const colorSpan = document.createElement('span');
-//colorSpan.textContent = 'Pick a Color: '
-
-const colorSpan=document.getElementById('color_wheel');
-
-const colors = document.createElement('input');
-colors.setAttribute('type', 'color');
-colors.setAttribute('value', '#000000');
-colorSpan.appendChild(colors);
-tools.appendChild(colorSpan);
-
-/*Resize etch-a-sketch feature, not yet implemented
-const sizeSpan = document.createElement('span');
-const size = document.createElement('input');
-sizeSpan.textContent ='Choose Grid Size: ';
-size.setAttribute('type', 'number');
-size.setAttribute('value', '16');
-size.setAttribute('min', 10);
-size.setAttribute('max', 50);
-size.classList.add('size');
-width = size.getAttribute('value');
-sizeSpan.appendChild(size);
-tools.appendChild(sizeSpan);
-*/
-
-/*
-const clearSpan = document.createElement('span');
-clearSpan.classList.add('clearSpan')
-const clear = document.createElement('button');
-clear.classList.add('clear');
-clear.textContent = 'clear';
-*/
-
+//clear button event listener
 const clear = document.getElementById('clear');
 clear.addEventListener('click', () =>{
   let changed = document.querySelectorAll('.select');
@@ -94,51 +63,26 @@ clear.addEventListener('click', () =>{
     changed[i].style.backgroundColor ='white';
   }
 });
-/*
-clearSpan.appendChild(clear);
-tools.appendChild(clearSpan);
-*/
-/*const resize = document.createElement('button');
-resize.classList.add('resize');
-resize.textContent = 'resize';
-resize.addEventListener('click', () =>{
-
-})*/
-
-
-/*
-//create knobs
-const leftKnob =document.createElement('div');
-leftKnob.classList.add('circle');
-leftKnob.classList.add('left');
-
-const rightKnob = document.createElement('div');
-rightKnob.classList.add('circle');
-rightKnob.classList.add('right');
-
-container.appendChild(leftKnob);
-container.appendChild(rightKnob);
-container.append(tools);
-*/
 
 //If mouse up anywhere on page, stop drawing
 document.addEventListener('mouseup', ()=>{
   drawing = false;
-})
-size.addEventListener('keypress', (e) =>{
-  if(e.key === 'Enter'){
-    width = size.value;
-    if(width < 16){
-      size.setAttribute('value', '16');
-      size.value = 16;
-      alert('Must be a size between 16 and 50. Try Again.');
-    }else if(width > 50){
-      alert('Must be a size between 16 and 50. Try Again.');
-      size.value = 50;
+});
+
+//resize event listener
+const resize = document.getElementById('resize');
+resize.addEventListener('click', ()=>{
+    let newWidth = size.value;
+    if(newWidth < minWidth){
+        alert(`Size must be greater than ${minWidth}`);
+        size.value = 16;
+    }else if(newWidth > 100){
+        alert(`Size must be less than ${maxWidth}`);
+        size.value = 16;
+    }else{
+        while(container.firstChild){
+          container.firstChild.remove();
+        }
+       createGrid(newWidth);
     }
-    let hidden = document.querySelectorAll('.hide');
-    for(let i = 0; i < hidden.length; i++){
-      hidden[i].classList.remove('hide');
-    }
-  }
-})
+});
